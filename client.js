1ddebf9120a629f7c51227b97fabff223a7bfda0
McKenna Lusk <mckenna.lusk7@gmail.com>
2020-03-13 17:20:16 -0500

    added function init

--- client.js
+++ client.js
@@ -1,3 +1,7 @@
 console.log("Whattuppp");
 
-// $(document).ready(init);
+$(document).ready(init);
+function init() {
+  $("js-total-salary").on("submit", submitEmployee);
+  $(".js-table-body").on("click", ".js-btn-delete", deleteEmployee);
+}
